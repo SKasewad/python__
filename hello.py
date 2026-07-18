fc7f1987346741a5e3cf4f188615ab4c459fdc99
Santoba kasewad <skasewad1908@gmail.com>
2026-07-18 15:47:41 +0530

    new changes

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello World")
+str1 = input("Enter first string:")
+str2 = input("Enter sec string:")
+if sorted(str1.lower()) == sorted(str2.lower()):
+    print("Anagram")
+else:
+    print("Not Allowed")
+    
