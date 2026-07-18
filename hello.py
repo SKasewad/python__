a641edb8986ed936ab9a3a0abe18a03caf062e18
Santoba kasewad <skasewad1908@gmail.com>
2026-07-18 17:52:03 +0530

    add new changes

--- hello.py
+++ hello.py
@@ -1,4 +1,19 @@
-print("Hello Santoba")
-print("I am good")
-print("How are you!")    
-print(3)
+# print("Hello Santoba")
+# print("I am good")
+# print("How are you!")    
+# print(3)
+age = 3
+print(age)
+print(type(age))
+
+name = "Santoba"
+print(name)
+print(type(name))
+
+cgpa = 8.05
+print(cgpa)
+print(type(cgpa))
+
+is_complited = True
+print(is_complited)
+print(type(is_complited))
