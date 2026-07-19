4519307d37cef4b150df28c98013991a0e66b005
Santoba kasewad <skasewad1908@gmail.com>
2026-07-19 12:12:11 +0530

    add new changes

--- hello.py
+++ hello.py
@@ -2,18 +2,34 @@
 # print("I am good")
 # print("How are you!")    
 # print(3)
-age = 3
-print(age)
-print(type(age))
+# age = 3
+# print(age)
+# print(type(age))
 
-name = "Santoba"
-print(name)
-print(type(name))
+# name = "Santoba"
+# print(name)
+# print(type(name))
 
-cgpa = 8.05
-print(cgpa)
-print(type(cgpa))
+# cgpa = 8.05
+# print(cgpa)
+# print(type(cgpa))
 
-is_complited = True
-print(is_complited)
-print(type(is_complited))
+# is_complited = True
+# print(is_complited)
+# print(type(is_complited))
+a = 34
+b = "34"
+d = 1908
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#Convert b into an integer
+c = int(b)
+print(c)
+print(type(c))
+
+e = str(d)
+print (e)
+print (type(e))
