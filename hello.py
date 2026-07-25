f56768acd2c249a4fe6c9f6ccfd975723108e54f
Santoba kasewad <skasewad1908@gmail.com>
2026-07-25 16:18:26 +0530

    Replace prize matching logic with a simple number printing loop in hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,2 @@
-a = int(input("Enter a number between 1 to 10: "))
-match a:
-    case 1:
-        print("You won a 3$")
-    case 3:
-        print("You won a doll")
-    case 7:
-        print("You got a refrigerator")
-    case _:
-        print("Better Luck next time")
+for i in range(1,11):
+    print( "5 X",i,"=",5*(i))
