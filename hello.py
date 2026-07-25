c06de07f97cb0452abd6f5923595b01a2d0d9f11
Santoba kasewad <skasewad1908@gmail.com>
2026-07-25 19:42:47 +0530

    Refactor code structure and remove unused loops in hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
-for i in range(1,11):
-    print( "5 X",i,"=",5*(i))
+# i = 2
+# while True:
+#     print(i)
+#     i = i + 1
+# for i in range(1,10):
+#     i = "Santoba"
+#     print(i)
+# for i in range(0,21):
+    
+#     if i == 11:
+    
+#         continue #Cancle the execution of this code
+#     print(i)
+
+i = 32
+if i == 21:
+    pass #do nothing
+    print("End of the program ")
+print("End of the program ")
