e43d47af02d0c999c3bea7a83cba0275c2be4347
Santoba kasewad <skasewad1908@gmail.com>
2026-07-30 17:39:17 +0530

    Comment out number and age input checks in hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,23 @@
-num = int(input("Enter your number:"))
-if(num>0):
-    print("The given no is positive")
-elif(num<0):
-    print("The given no is negative ")
-elif(num == 0):
-    print("The given no is zero")
-else:
-    print("Imagenary")
+# num = int(input("Enter your number:"))
+# if(num>0):
+#     print("The given no is positive")
+# elif(num<0):
+#     print("The given no is negative ")
+# elif(num == 0):
+#     print("The given no is zero")
+# else:
+#     print("Imagenary")
 
 
-age = int(input("Enter your age:"))
-if(age>=18):
-    print("Your eligible to vote")
-else:
-    print("Your not eligible")
+# age = int(input("Enter your age:"))
+# if(age>=18):
+#     print("Your eligible to vote")
+# else:
+#     print("Your not eligible")
 
 
-num = int(input("Enter your Number:"))
-if(num%2==0):
-    print("The no is even")
-else:
-    print("The no is odd")
+# num = int(input("Enter your Number:"))
+# if(num%2==0):
+#     print("The no is even")
+# else:
+#     print("The no is odd")
