aad27c23e690191a9fa7611743623eb186be9434
Santoba kasewad <skasewad1908@gmail.com>
2026-08-01 12:22:37 +0530

    Comment out loop structures for number iteration in hello.py

--- hello.py
+++ hello.py
@@ -84,10 +84,22 @@
 # print("You sucessfully loged in ")
 
 
-for i in range (1,11):
-    if i == 8:
-        break
-    print(i)
+# for i in range (1,11):
+#     if i == 8:
+#         break
+#     print(i)
+
+
+# for i in range (1,6):
+#     if i == 3:
+#        pass
+#     print(i)
+
+
+# for i in range (1,11):
+#     if i == 5:
+#        continue
+#     print(i)
 
 
 
