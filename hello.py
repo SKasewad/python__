a64ecdc7f45c45f05ac79f57101039405b8b9ca3
Santoba kasewad <skasewad1908@gmail.com>
2026-08-03 22:24:12 +0530

    Comment out template string and related variables in hello.py

--- hello.py
+++ hello.py
@@ -154,14 +154,14 @@
 # print(text.isspace())
 
 
-template = "Dear {},You are awesome. Take this {}$ bag "
-a = "John"
-a1 = 10000
-b = "Jack"
-b1 = 1000
-c = "Marie"
-c2 = 300
-s1 = template.format(a,a1)
-print(s1)
-
+# template = "Dear {},You are awesome. Take this {}$ bag "
+# a = "John"
+# a1 = 10000
+# b = "Jack"
+# b1 = 1000
+# c = "Marie"
+# c2 = 300
+# s1 = template.format(a,a1)
+# print(s1)
+# print(f"{a} you are awesome, take this {b1}$ bag")
 
