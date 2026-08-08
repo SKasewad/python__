8abdc175b35fdc3cbb27a2effac61ef824f83ca5
Santoba kasewad <skasewad1908@gmail.com>
2026-08-08 23:46:28 +0530

    Remove average function and related print statements from hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,50 @@
-def average(a,b,c):
-    d = (a + b + c)/3
-    # print(d)
-    return d
+# def add(a,b,plus=0):
+#     x = a+b
+#     return x
+# c = add(3,4,2)
+# c1 = add(b=5,a=9)
+# print(c)
+# print(c1)
 
-f1=average(3,2,5)
-f2=average(3,4,7)
-print(f1)
-print(f2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+square = lambda x : x*x
+sum = lambda x,y: x+y
+print(square(3))
+print(sum(34,43))
+
+
+
+
+
+
+
+
+
+
+
+# def add(a,b):
+#     a = a+b
+#     return a
+# a = add(3,2)
+# a1 = add(b=11,a=33)
+# print(a)
+# print(a1)
