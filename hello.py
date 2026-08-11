ad950fc698ad2b05a0ead410d1e030a55665a180
Santoba kasewad <skasewad1908@gmail.com>
2026-08-11 22:43:08 +0530

    Refactor hello.py to move variable initialization and add greet function

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 def sum(a,b):
     #a&b are the local variable..
     c = a+b
-    print(z)
+    z = 1
     return c
     
-
+def greet():
+    z = 32 #local variable
+    print("Hello")
 z = 8 #z is a global variable
 print(sum(9,10))
+print(z)
     
