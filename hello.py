fa07a09bc585fe83991b87bc9907381187a45ade
Santoba kasewad <skasewad1908@gmail.com>
2026-08-11 22:46:33 +0530

    Refactor sum function in hello.py to remove unused variables and clean up code

--- hello.py
+++ hello.py
@@ -1,13 +1,9 @@
 def sum(a,b):
-    #a&b are the local variable..
-    c = a+b
-    z = 1
+    print("Hey I am summing")
+    c = a + b
+    z = 0
     return c
-    
-def greet():
-    z = 32 #local variable
-    print("Hello")
-z = 8 #z is a global variable
-print(sum(9,10))
+
+z = 3
+print(sum(3,11))
 print(z)
-    
