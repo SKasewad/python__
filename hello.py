250de366ae325a5e42a89840cb76345de32a8c9f
Santoba kasewad <skasewad1908@gmail.com>
2026-08-12 23:24:47 +0530

    Refactor hello.py to comment out multiple unused functions for clarity

--- hello.py
+++ hello.py
@@ -1,41 +1,7 @@
-# def square(x):
-#     return x*x
-# print(square(3))
 
-
-# def full_name(first,last):
-#     return f"{first} {last}"
-# print(full_name("Santoba","Kasewad"))
-
-# def area(length,width=10):
-#     return length*width
-# print(f"The area of this reactangle is",area(10,10))
-# print(f"The area of this reactangle is",area(13))
-
-
-# add = lambda a,b :a+b
-# print(add(10,15))
-
-# square =  lambda x: x*x
-# list1 = [1,2,3,4,5]
-# print(list(map(square,list1)))
-
-
-# def factorial(n):
-#     if n==0 or n==1:
-#         return 1
-#     return factorial (n-1)*n
-# print(factorial(5))
-
-
-# def sum_of_digits(n):
-#     if n == 0:
-#         return 0
-#     return n%10 + sum_of_digits(n//10)
-# print(sum_of_digits(123))
 
 
 import math
-a=math.sqrt(16)
-b=math.sin(math.radians(90))
+a = math.sqrt(16)
+b = math.sin(math.radians(90))
 print(a,b)
