adbc065e6feef1c432fd33b8c5ebf40f0a5dff75
Santoba kasewad <skasewad1908@gmail.com>
2026-08-12 18:17:19 +0530

    Refactor hello.py to comment out square, factorial, and sum_of_digits functions for clarity

--- hello.py
+++ hello.py
@@ -16,6 +16,26 @@
 # add = lambda a,b :a+b
 # print(add(10,15))
 
-square =  lambda x: x*x
-list1 = [1,2,3,4,5]
-print(list(map(square,list1)))
+# square =  lambda x: x*x
+# list1 = [1,2,3,4,5]
+# print(list(map(square,list1)))
+
+
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     return factorial (n-1)*n
+# print(factorial(5))
+
+
+# def sum_of_digits(n):
+#     if n == 0:
+#         return 0
+#     return n%10 + sum_of_digits(n//10)
+# print(sum_of_digits(123))
+
+
+import math
+a=math.sqrt(16)
+b=math.sin(math.radians(90))
+print(a,b)
