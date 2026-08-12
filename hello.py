878cf795866b6a7070347b2e389fb4e7c58ccd30
Santoba kasewad <skasewad1908@gmail.com>
2026-08-12 18:02:26 +0530

    Refactor hello.py to comment out unused functions and retain lambda implementation for square

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
-def square(x):
-    return x*x
-print(square(3))
+# def square(x):
+#     return x*x
+# print(square(3))
 
 
-def full_name(first,last):
-    return f"{first} {last}"
-print(full_name("Santoba","Kasewad"))
+# def full_name(first,last):
+#     return f"{first} {last}"
+# print(full_name("Santoba","Kasewad"))
 
+# def area(length,width=10):
+#     return length*width
+# print(f"The area of this reactangle is",area(10,10))
+# print(f"The area of this reactangle is",area(13))
+
+
+# add = lambda a,b :a+b
+# print(add(10,15))
+
+square =  lambda x: x*x
+list1 = [1,2,3,4,5]
+print(list(map(square,list1)))
