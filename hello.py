1ca33aa63250be70b4f3b85f070ac45b25fae63e
Santoba kasewad <skasewad1908@gmail.com>
2026-08-13 17:36:28 +0530

    Refactor hello.py to remove API request code and retain list operations

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-import requests
-a = requests.get("https://api.github.com/")
-print(a.json())
+marks = [23,34,76,89]
+mixed = [3.14, "Hello", False,43]
 
-
-
+print(marks[::-1])
+print(mixed[3])
+print(marks[3])
