0a858077b0b7f3addcb0e61f480609a72ff51781
Santoba kasewad <skasewad1908@gmail.com>
2026-08-13 18:26:49 +0530

    Refactor hello.py to append and extend lists, updating print statements accordingly

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 marks = [23,34,76,89]
 mixed = [3.14, "Hello", False,43]
-
-print(marks[::-1])
-print(mixed[3])
+marks.append(63)
+mixed.append(54)
+marks.extend(mixed)
+print(marks)
+print(mixed)
 print(marks[3])
