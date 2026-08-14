3474bafd75bd60da324643727557dca738f110b3
Santoba kasewad <skasewad1908@gmail.com>
2026-08-14 17:35:00 +0530

    Refactor hello.py to comment out unused code and improve clarity

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
-a = {12,43,23,54,66,23,89}
+# a = {12,43,23,54,66,23,89}
+# print(a)
+# print(type(a))
+# print(a[3])
+# #an error is showing that the set is not an ordered collections of items. It is something lke a basquate in that randomly stored the items in the same way items are stored in the set in python... You got it nigga..
+
+a = {12,32,45,66}
+a.add(43)
+a.add(2324)
+a.remove(32)
+# a.remove(548756) #This is throws is an error
+a.discard(5436776) #This not showing any type of error if the element is present in that set that will be remove if not then its move towards the code..
 print(a)
-print(type(a))
-print(a[3])
-#an error is showing that the set is not an ordered collections of items. It is something lke a basquate in that randomly stored the items in the same way items are stored in the set in python... You got it nigga..
