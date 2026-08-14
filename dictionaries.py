cb22aed357d250d36a15426e69ac40a1f25b89a2
Santoba kasewad <skasewad1908@gmail.com>
2026-08-14 18:30:03 +0530

    dictionaries and its methods

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+marks = {"Santoba":34,"Gayatri":43, "Suraj":55,"Aditya":29}
+print(marks,type(marks))
+print(marks["Santoba"])
+marks["Santoba"] = 23
+print(marks)
+print(marks.keys())
+print(marks.values())
