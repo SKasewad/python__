bc4e4faf7adedf2466472c60fd5d63214121af72
Santoba kasewad <skasewad1908@gmail.com>
2026-08-14 17:17:29 +0530

    uses of index and count in tuples

--- hello.py
+++ hello.py
@@ -1,11 +1,3 @@
-# a = (3,2,22,13)
-# single_element = (5,)
-# print(a)
-# print(a[2])
-# # a[3] = 32
-# print(single_element)
-
-
-tu = (1,2,3)
-a,b,c = tu
-print(a,b,c)
+a = (12,43,23,54,66,23,89)
+print(a.count(23))
+print(a.index(23))
