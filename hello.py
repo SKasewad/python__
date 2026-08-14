152c0e43dadc856372cec027b941441e64abfbe0
Santoba kasewad <skasewad1908@gmail.com>
2026-08-14 17:25:25 +0530

    set is a unordered collection

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-a = (12,43,23,54,66,23,89)
-print(a.count(23))
-print(a.index(23))
+a = {12,43,23,54,66,23,89}
+print(a)
+print(type(a))
+print(a[3])
+#an error is showing that the set is not an ordered collections of items. It is something lke a basquate in that randomly stored the items in the same way items are stored in the set in python... You got it nigga..
