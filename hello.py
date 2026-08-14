82839fdcca176fa993b3d688aa928b7648354c80
Santoba kasewad <skasewad1908@gmail.com>
2026-08-14 16:50:56 +0530

    Refactor hello.py to comment out table generation code for clarity

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 #create a list containing a table of 5
-a = 5
+# a = 5
 
-table = []
+# table = []
 
-for i in range(1,11):
+# for i in range(1,11):
 
-    table.append(5*i)
-    
-print(table)
+#     table.append(5*i)
+
+# print(table)
+squared = []
+squared = [i**2 for i in range(11)]
+print(squared)
